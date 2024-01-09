307475c656b69dec2c6a5a28ee043646732731cc
SyydMR <mohammadrezadaryabaki@gmail.com>
2024-01-08 18:39:35 -0700

    update panel managment

--- statics/base-style/js/app.js
+++ statics/base-style/js/app.js
@@ -22,3 +22,17 @@ function functionName(){
 function removeMessage() {
     $('#message_container').fadeOut('fast');
 }
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
+
+togglePassword.addEventListener("click", function () {
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+    
+    this.classList.toggle("bi-eye");
+});
+
+const form = document.querySelector("form");
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+});
--- statics/panel-style/js/app.js
+++ statics/panel-style/js/app.js
@@ -12,17 +12,3 @@ function showList() {
 function showField() {
   searchField.classList.toggle("show");
 }
-const togglePassword = document.querySelector("#togglePassword");
-const password = document.querySelector("#password");
-
-togglePassword.addEventListener("click", function () {
-    const type = password.getAttribute("type") === "password" ? "text" : "password";
-    password.setAttribute("type", type);
-    
-    this.classList.toggle("bi-eye");
-});
-
-const form = document.querySelector("form");
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-});
